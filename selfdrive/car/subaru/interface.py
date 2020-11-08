5cd640a5d4a18950f58a72d73b81e28c6c7e91a5
Martin Lillepuu <martin@mlp.ee>
2020-11-08 11:16:35 +0200

    Comment out preglobal dashcamOnly

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -25,7 +25,7 @@ class CarInterface(CarInterfaceBase):
     # Subaru port is a community feature, since we don't own one to test
     ret.communityFeature = True
 
-    ret.dashcamOnly = candidate in PREGLOBAL_CARS
+    #ret.dashcamOnly = candidate in PREGLOBAL_CARS
 
     # force openpilot to fake the stock camera, since car harness is not supported yet and old style giraffe (with switches)
     # was never released
