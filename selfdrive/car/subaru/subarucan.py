48f7ac68887e554bd3f15c8ec555586c9d2b6e35
Alex van Stekelenborg <alex.vanstek@gmail.com>
2021-01-22 09:37:53 -0500

    Update subarucan.py

--- selfdrive/car/subaru/subarucan.py
+++ selfdrive/car/subaru/subarucan.py
@@ -27,6 +27,29 @@ def create_es_distance(packer, es_distance_msg, pcm_cancel_cmd):
 
   return packer.make_can_msg("ES_Distance", 0, values)
 
+#------------------TRICK NON-EPB------------------
+def create_brake_pedal(packer, brake_pedal_msg, fake_wheel_speed):
+
+  values = copy.copy(brake_pedal_msg)
+  #Only modify Speed if the fake speed is not -1
+  if fake_wheel_speed != -1:
+    values["Speed"] = fake_wheel_speed
+
+  return packer.make_can_msg("Brake_Pedal", 2, values)   
+#------------------------------------------------- 
+
+#---------------SUBARU STOP AND GO-----------------
+def create_throttle(packer, throttle_msg, throttle_cmd):
+
+  values = copy.copy(throttle_msg)
+  #Only modify throttle_pedal if command is not -1, otherwise, just bypass/forward original message
+  #For safety, limit our throttle command to 20 (out of 255)
+  if throttle_cmd != -1 and throttle_cmd <= 10:
+    values["Throttle_Pedal"] = throttle_cmd
+
+  return packer.make_can_msg("Throttle", 2, values)
+#---------------------------------------------------  
+
 def create_es_lkas(packer, es_lkas_msg, visual_alert, left_line, right_line):
 
   values = copy.copy(es_lkas_msg)
@@ -43,6 +66,17 @@ def create_es_lkas(packer, es_lkas_msg, visual_alert, left_line, right_line):
 def subaru_preglobal_checksum(packer, values, addr):
   dat = packer.make_can_msg(addr, 0, values)[2]
   return (sum(dat[:7])) % 256
+
+def create_preglobal_throttle_control(packer, throttle_msg, throttle_cmd):
+
+  values = copy.copy(throttle_msg)
+
+  if throttle_cmd != -1 and throttle_cmd <= 10:
+    values["Throttle_Pedal"] = throttle_cmd
+
+  #values["Checksum"] = subaru_preglobal_checksum(packer, values, "Throttle")
+
+  return packer.make_can_msg("Throttle", 2, values)  
 
 def create_preglobal_steering_control(packer, apply_steer, frame, steer_step):
 
